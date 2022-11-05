75c8be838dab4f9a1457401391862f6bd542a0b2
mmertdogann <mm3rtdogann@gmail.com>
2022-11-05 17:07:54 +0100

    Add test

---
+++ controllers/dummy_controller_test.go
@@ -0,0 +1,59 @@
+package controllers
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	anyninesv1 "github.com/mmertdogann/dummy-operator/api/v1"
+)
+
+var _ = Describe("Dummy controller", func() {
+
+	// Define utility constants for object names and testing timeouts/durations and intervals.
+	const (
+		DummyName      = "test-dummy"
+		DummyNamespace = "default"
+		timeout        = time.Second * 10
+		interval       = time.Millisecond * 250
+	)
+
+	Context("When creating Dummy", func() {
+		It("Should check CR specs", func() {
+			By("By creating a new Dummy")
+			ctx := context.Background()
+			dummy := &anyninesv1.Dummy{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "anynines.interview.com/v1",
+					Kind:       "Dummy",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      DummyName,
+					Namespace: DummyNamespace,
+				},
+				Spec: anyninesv1.DummySpec{
+					Message: "I'm just a dummy",
+				},
+			}
+			Expect(k8sClient.Create(ctx, dummy)).Should(Succeed())
+
+			dummyLookupKey := types.NamespacedName{Name: DummyName, Namespace: DummyNamespace}
+			createdDummy := &anyninesv1.Dummy{}
+
+			// We'll need to retry getting this newly created Dummy, given that creation may not immediately happen.
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, dummyLookupKey, createdDummy)
+				if err != nil {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+			// Let's make sure our Schedule string value was properly converted/handled.
+			Expect(createdDummy.Spec.Message).Should(Equal("anynines.interview.com/v1"))
+		})
+	})
+})
--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.19
 
 require (
 	github.com/go-logr/logr v1.2.3
+	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/ginkgo/v2 v2.1.4
 	github.com/onsi/gomega v1.19.0
+	go.uber.org/zap v1.21.0
 	k8s.io/api v0.25.0
 	k8s.io/apimachinery v0.25.0
 	k8s.io/client-go v0.25.0
@@ -48,6 +50,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.12.2 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
@@ -56,7 +59,6 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
@@ -68,6 +70,7 @@ require (
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.25.0 // indirect
